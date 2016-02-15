5f037ef02f347398c22b3ac908a45f051fc9285a
Kyle Aleshire <kjaleshire@gmail.com>
2016-02-15 14:20:51 -0600

    Add vanilla arm formulae

---
+++ arm-none-binutils.rb
@@ -0,0 +1,23 @@
+require 'formula'
+
+class ArmElfBinutils < Formula
+  homepage 'http://gcc.gnu.org'
+  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.26.tar.gz'
+  sha256 '9615feddaeedc214d1a1ecd77b6697449c952eab69d79ab2125ea050e944bcc1'
+
+  depends_on 'gcc' => :build
+  def install
+    ENV['CC'] = '/usr/local/opt/gcc/bin/gcc-5'
+    ENV['CXX'] = '/usr/local/opt/gcc/bin/g++-5'
+    ENV['CPP'] = '/usr/local/opt/gcc/bin/cpp-5'
+    ENV['LD'] = '/usr/local/opt/gcc/bin/gcc-5'
+
+    mkdir 'build' do
+      system '../configure', '--disable-nls', '--target=arm-none-eabi','--disable-werror',
+                             '--enable-gold=yes',
+                             "--prefix=#{prefix}"
+      system 'make all'
+      system 'make install'
+    end
+  end
+end
---
+++ arm-none-gcc.rb
@@ -0,0 +1,36 @@
+require 'formula'
+
+class ArmElfGcc < Formula
+  homepage 'http://gcc.gnu.org'
+  url "http://ftpmirror.gnu.org/gcc/gcc-5.3.0/gcc-5.3.0.tar.bz2"
+  mirror "https://ftp.gnu.org/gnu/gcc/gcc-5.3.0/gcc-5.3.0.tar.bz2"
+  sha256 "b84f5592e9218b73dbae612b5253035a7b34a9a1f7688d2e1bfaaf7267d5c4db"
+
+  depends_on 'gmp'
+  depends_on 'libmpc'
+  depends_on 'mpfr'
+  depends_on 'arm-none-binutils'
+
+  def install
+    binutils = Formula.factory 'arm-none-binutils'
+
+    ENV['CC'] = '/usr/local/opt/gcc/bin/gcc-5'
+    ENV['CXX'] = '/usr/local/opt/gcc/bin/g++-5'
+    ENV['CPP'] = '/usr/local/opt/gcc/bin/cpp-5'
+    ENV['LD'] = '/usr/local/opt/gcc/bin/gcc-5'
+    ENV['PATH'] += ":#{binutils.prefix/"bin"}"
+
+    mkdir 'build' do
+      system '../configure', '--disable-nls', '--target=arm-none-eabi', '--disable-werror',
+                             "--prefix=#{prefix}",
+                             "--enable-languages=c,c++,objc,obj-c++",
+                             "--without-headers"
+      system 'make all-gcc'
+      system 'make install-gcc'
+      FileUtils.ln_sf binutils.prefix/"arm-none-eabi", prefix/"arm-none-eabi"
+      system 'make all-target-libgcc'
+      system 'make install-target-libgcc'
+      FileUtils.rm_rf share/"man"/"man7"
+    end
+  end
+end
